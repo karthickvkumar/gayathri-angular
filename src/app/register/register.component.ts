c11cf4402a8ce51568ee32a52ce948b2ac8a6751
kvelraj_concord <kvelraj@concord.net>
2020-10-27 08:48:34 +0530

    update

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  registerData : any = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    dob: '',
+    gender: '',
+    hobbies: [],
+    address: '',
+    state: ''
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onChangeHobbies(event, index){
+    console.log(event.target.value, event.target.checked, index)
+    
+    if(event.target.checked){
+      this.registerData.hobbies.push(event.target.value)
+    }
+    else{
+      let index = this.registerData.hobbies.findIndex(value => value == event.target.value)
+      this.registerData.hobbies.splice(index, 1);
+    }
+    
+    console.log(this.registerData)
+  }
+
+  onRegister(){
+    console.log(this.registerData)
+  }
 }
